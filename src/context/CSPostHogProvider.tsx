bd975709b82954f68507f80d8ff6335698c377d8
Denil Bhatt <denilbhatt2004@gmail.com>
2024-07-17 22:22:43 +0530

    added posthog monitoring

--- src/context/CSPostHogProvider.tsx
+++ src/context/CSPostHogProvider.tsx
@@ -3,13 +3,13 @@ import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 
 if (typeof window !== "undefined") {
-  if (process.env.NODE_ENV != "production")
+  if (process.env.NODE_ENV == "production")
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
       person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
     });
 }
 export function CSPostHogProvider({ children }: { children: React.ReactNode }) {
-  if (process.env.NODE_ENV != "production") return <>{children}</>;
+  if (process.env.NODE_ENV == "production") return <>{children}</>;
   return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
 }
